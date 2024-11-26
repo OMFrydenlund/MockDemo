312efd4df0a41fcaf0c4ada205cfc54a4437294f
OMFrydenlund <migfrydenlund@gmail.com>
2024-11-26 14:51:32 +0100

    Removed studentAdmin repo and created new file, planned refactor in progress

---
+++ StudentAdministrering/Program.cs
@@ -0,0 +1,13 @@
+﻿using StudentAdministrering;
+int gitteID = 0;
+
+Student bobby = new Student();
+bobby.SpørOmNavn();
+bobby.SpørOmAlder();
+bobby.SpørOmStudieRetning();
+bobby.GiEnID();
+gitteID++;
+
+bobby.SkrivUtInfo();
+
+Console.ReadLine();
---
+++ StudentAdministrering/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAdministrering
+{
+    internal class Student
+    {
+        public string _studentNavn { get; private set; }
+        public int _studentAlder { get; private set; }
+        public string _studieRetning { get; private set; }
+        public int _studentID { get; set; }
+
+        public string SpørOmNavn()
+        {
+            Console.Clear();
+            Console.Write("Hva heter studenten? ");
+            string navnInput = Console.ReadLine();
+            return navnInput;
+        }
+
+        public int SpørOmAlder()
+        {
+            Console.Clear();
+            Console.Write("Hvor gammel studenten? ");
+            int.TryParse(Console.ReadLine(), out int alderInput);
+            return alderInput;
+        }
+
+        public string SpørOmStudieRetning()
+        {
+            Console.Clear();
+            Console.Write("Hvilken studieretning skal tas opp? ");
+            string retningInput = Console.ReadLine();
+            return retningInput;
+        }
+
+        public int GiEnID()
+        {
+            Random random = new Random();
+            int nyID = random.Next(1, 250);
+            return nyID;
+        }
+        public void SkrivUtInfo()
+        {
+            Console.Clear();
+            Console.WriteLine($"Navn: {_studentNavn}\nAlder: {_studentAlder}\nProgram: {_studieRetning}\nID nummer: {_studentID}");
+        }
+    }
+}
