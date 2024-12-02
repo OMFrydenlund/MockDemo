7d61a9d98fb6f34d4002e8e0da0136b35331d80b
OMFrydenlund <migfrydenlund@gmail.com>
2024-12-02 14:47:37 +0100

    KommentarHUH moments

--- StudentAdministrering/Program.cs
+++ StudentAdministrering/Program.cs
@@ -1,9 +1,16 @@
 ﻿using StudentAdministrering;
 StudentAdminApp app = new StudentAdminApp();
 
-// mangler:
+// mangler (iht oppgave + ekstra):
 //      * valg/behandling av gjennomsnittskarakter-beregning
 //      * valg/behandling for totalsum av studiepoeng
+//      * valg for å gå tilbake til hovedmeny fra alle menyer(?)
 
-// edge cases:
-//      * trenger konsoll beskjed om ikke en spesifikk liste ikke har objekter
+// done-did-it-list
+//      * får brukeren beskjed om det er ingen registrerte studenter
+//      * får brukeren beskjed om det ikke er fag satt opp
+//      * kan brukeren få valg på nytt i alle menyene, om de taster feil (hva ser de)
+//      * gir error rerouting mening når brukeren taster feil? (f eks. de går tilbake til undermenyen og ikke helt tilbake til hovedmeny)
+//      * beholder listene verdier som er satte inn:
+//          - hvis man taster feil
+//          - skriver ut, for så å legge til nye og skrive ut igjen
